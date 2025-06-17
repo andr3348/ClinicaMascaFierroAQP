ac7b9c3e5e2431c15e3bb55c8001e87155f957ee
andree <andree1211val@gmail.com>
2025-06-16 22:40:18 -0500

    error de tailwindCSS arreglado

--- views/login/login.php
+++ views/login/login.php
@@ -1,29 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>Log In</title>
-</head>
-<body>
-    <div class="container">
-        <div>
-            <label for="correo" class="title">INICIAR SESIÓN</label><br><br>
-            <form action="../../controllers/UsuarioController.php?action=login" method="POST">
-                <label for="correo">Correo:</label>
-                <input type="email" name="correo" id="correo" required>
-                <label for="contraseña">Contraseña:</label>
-                <input type="password" name="password" id="contraseña" required>
-                <button type="submit">Iniciar Sesión</button>
-            </form>
-        </div>
-        <a href="../registrar/registrar.html">Registrarse</a>
-        <?php 
-        if (isset($_GET['error']) && $_GET['error'] === '1') {
-        echo '<br><br><p style="color: red;">Usuario no encontrado<p>';
-        }
-        ?>
+<div class="flex justify-center">
+    <div>
+        <label for="correo" class="">INICIAR SESIÓN</label><br><br>
+        <form action="../../controllers/UsuarioController.php?action=login" method="POST">
+            <label for="correo">Correo:</label>
+            <input type="email" name="correo" id="correo" required>
+            <label for="contraseña">Contraseña:</label>
+            <input type="password" name="password" id="contraseña" required>
+            <button type="submit">Iniciar Sesión</button>
+        </form>
     </div>
-</body>
-</html>
+    <a href="../registrar/registrar.html">Registrarse</a>
+    <?php
+    if (isset($_GET['error']) && $_GET['error'] === '1') {
+        echo '<br><br><p style="color: red;">Usuario no encontrado<p>';
+    }
+    ?>
+</div>
